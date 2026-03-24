cbaa2dbdc6ea5240a9caf5195d9ae0e88f77be38
Jeremie Berduck <jeremieberduck@gmail.com>
2026-03-24 18:39:12 +0100

    Test App Example

---
+++ App Example/App Example/App_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ExampleApp.swift
+//  App Example
+//
+//  Created by Jeremie Berduck on 24/3/26.
+//
+
+import SwiftUI
+
+@main
+struct App_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
